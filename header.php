a67d9394cf1fa9dedc1e3544ede6fc85ea260d04
jjjjota <jajopia@uc.cl>
2018-07-26 01:45:47 -0400

    :egg: Added metas and navbar to header.php

--- header.php
+++ header.php
@@ -1,1 +1,44 @@
 <?php wp_head(); ?>
+
+<!DOCTYPE html>
+<html>
+
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Desafío 17</title>
+</head>
+
+
+<body>
+  <!-- NAVBAR ------------------------------------------------------------------------------------->
+  <nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+
+      <!-- Brand and toggle get grouped for better mobile display -->
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <img class="navbar-brand" src="images/latam_logo.png"/>
+      </div>
+
+      <!-- Collect the nav links, forms, and other content for toggling -->
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="#">Equipo</a></li>
+          <li><a href="#">Blog</a></li>
+          <li><a href="#contacto">Contacto</a></li>
+        </ul>
+      </div><!-- /.navbar-collapse -->
+
+    </div><!-- /.container-fluid -->
+  </nav>
+</body>
+
+
+</html>
