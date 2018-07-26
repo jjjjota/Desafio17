cfad6d27ddbac64d4746e9dbc9df67ae2253535f
jjjjota <jajopia@uc.cl>
2018-07-26 01:47:17 -0400

    :egg: Added footer to footer.php

--- footer.php
+++ footer.php
@@ -1,1 +1,8 @@
 <?php wp_footer(); ?>
+
+<body>
+  <!-- FOOTER ------------------------------------------------------------------------------------->
+  <footer>
+    <h3>DESAFIO LATAM 2018 - CURSO FRONTEND G9</h3>
+  </footer>
+</body>
