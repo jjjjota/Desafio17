330e6104ca68fef36b85bb056854382b722cfba2
jjjjota <jajopia@uc.cl>
2018-07-29 19:05:52 -0400

    :baby_chick: Included featured-image function on single.php

--- functions.php
+++ functions.php
@@ -28,4 +28,10 @@
   }
 
   add_action( 'wp_enqueue_scripts', 'dl_enqueue_scripts' );
+
+  function mytheme_post_thumbnails() {
+    add_theme_support( 'post-thumbnails' );
+  }
+
+  add_action( 'after_setup_theme', 'mytheme_post_thumbnails' );
 ?>
--- js/main.js
+++ js/main.js
@@ -1,12 +1,19 @@
-window.onscroll = function() {myFunction()};
+( function($) {
 
-function myFunction() {
-  var navbar = document.getElementsByTagName("nav")[0];
-  
-  if (document.body.scrollTop > 350 || document.documentElement.scrollTop > 350) {
-    navbar.classList.remove("navbar-transparent");
+  // Variables de selectores
+  var navbar = $("nav"),
+      navbarCollapse = $(".navbar-collapse");
+
+  // navbar: change bcolor on scroll
+  window.onscroll = function() {
+
+    if (document.body.scrollTop > 350 || document.documentElement.scrollTop > 350) {
+      navbar.removeClass("navbar-transparent");
+    }
+    else {
+      navbar.addClass("navbar-transparent");
+    }
   }
-  else {
-    navbar.classList.add("navbar-transparent");
-  }
-}
+
+
+}) (jQuery);
